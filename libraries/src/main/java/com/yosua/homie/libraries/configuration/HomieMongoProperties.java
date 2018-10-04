9d422d5cb8ad95ee5fdce628554c313d7b6f2102
Kenneth Wilson <kennethwilson100300@gmail.com>
2018-10-04 14:56:52 +0700

    edit mongo config

--- libraries/src/main/java/com/yosua/homie/libraries/configuration/HomieMongoProperties.java
+++ libraries/src/main/java/com/yosua/homie/libraries/configuration/HomieMongoProperties.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties("home.security.mongo")
+@ConfigurationProperties("homie.mongo")
 public class HomieMongoProperties {
   private Integer connectionPerHost;
   private Integer minConnectionsPerHost;
